085cbf503f572f985f064ff75b6858e27ef9e520
Marko <mantolic@tvz.hr>
2023-04-04 21:30:14 +0200

    Projektni

---
+++ Projektni_zadatak.py
@@ -0,0 +1,115 @@
+from datetime import date
+
+korisnici = []
+kategorije = []
+prodaje = []
+
+adrese = []
+
+broj_adresa = int(input("Unesite broj adresa: "))
+
+for i in range(1, broj_adresa + 1):
+    adresa = {}
+    adresa["Ulica"] = input(f"Unesite naziv ulice {i}. adrese: ")
+    adresa["Kucni_broj"] = int(input(f"Unesite kucni broj {i}. adrese: "))
+    adresa["Grad"] = input(f"Unesite naziv grada {i}. adrese: ")
+    adrese.append(adresa)
+
+
+broj_korisnika = int(input("Unesite broj korisnika: "))
+
+for i in range(1, broj_korisnika + 1):
+    korisnik = {}
+    korisnik["Ime"] = input(f"Unesite ime {i}. korisnika: ").capitalize()
+    korisnik["Prezime"] = input(f"Unesite prezime {i}. korisnika: ").capitalize()
+    korisnik["Telefon"] = int(input(f"Unesite telefon {i}. korisnika: "))
+    korisnik["Email"] = input(f"Unesite email {i}. korisnika: ").strip()
+
+    print("Popis adresa: ")
+    for j, adresa in enumerate(adrese, start=1):
+        print(f'{j}. adresa: {adresa["Grad"]}, {adresa["Ulica"]}')
+
+    odabrana_adresa = int(input("Odabrana adresa: "))
+
+    korisnik["Adresa"] = adrese[odabrana_adresa - 1]
+
+    korisnici.append(korisnik)
+
+
+broj_kategorija = int(input("Unesite broj kategorija: "))
+
+for i in range(1, broj_kategorija + 1):
+    kategorija = {}
+    artikli = []
+
+    kategorija["Naziv"] = input(f"Unesite naziv {i}. kategorije: ")
+
+    broj_artikala = int(input(f"Unesite broj artikala za {i}. kategoriju: "))
+    for j in range(1, broj_artikala + 1):
+        artikl = {}
+        artikl["Naslov"] = input(f"Unesite naslov {j}. artikla: ")
+        artikl["Opis"] = input(f"Unesite opis {j}. artikla: ")
+        artikl["Cijena"] = float(input(f"Unesite cijenu {j}. artikla: "))
+        artikli.append(artikl)
+
+    kategorija["Artikli"] = artikli
+    kategorije.append(kategorija)
+
+
+broj_prodaja = int(input("Unesite broj prodaja: "))
+
+for i in range(1, broj_prodaja + 1):
+    prodaja = {}
+
+    dan = int(input(f"Unesite dan isteka {i}. prodaje: "))
+    mjesec = int(input(f"Unesite mjesec isteka {i}. prodaje: "))
+    godina = int(input(f"Unesite godinu isteka {i}. prodaje: "))
+    prodaja["Datum"] = date(godina, mjesec, dan)
+
+#Odabir korisnika
+    print(f"Odaberite korisnika {i}. prodaje: ")
+    for j, korisnik in enumerate(korisnici, start=1):
+        print(f'\t{j}. {korisnik["Ime"]} {korisnik["Prezime"]}')
+
+    odabrani_korisnik = int(input("Odabrani korisnik: "))
+
+#Odabir kategorije
+    print(f"Odaberite kategoriju {i}. prodaje: ")
+    for j, kategorija in enumerate(kategorije, start=1):
+        print(f'\t{j}. {kategorija["Naziv"]}')
+
+    odabrana_kategorija = int(input("Odabrana kategorija: "))
+
+#Odabir artikla
+    print(f"Odaberite artikl {i}. prodaje: ")
+
+    for j, artikl in enumerate(kategorije[odabrana_kategorija-1]["Artikli"], start=1):
+        print(f'\t{j}. {kategorije[odabrana_kategorija-1]["Artikli"][j-1]["Naslov"]}')
+
+    odabrani_artikl = int(input("Odabrani artikl: "))
+
+    prodaja["Korisnik"] = korisnici[odabrani_korisnik - 1]
+    prodaja["Artikl"] = kategorije[odabrana_kategorija - 1]["Artikli"][odabrani_artikl - 1]
+    prodaje.append(prodaja)
+
+
+for i, prodaja in enumerate(prodaje, start=1):
+    print(f"Prodaja {i}: ")
+    print('Informacije o korisniku: ')
+    print(f'\tIme: {prodaja["Korisnik"]["Ime"]}')
+    print(f'\tPrezime: {prodaja["Korisnik"]["Prezime"]}')
+    print(f'\tTelefon: {prodaja["Korisnik"]["Telefon"]}')
+    print(f'\tEmail: {prodaja["Korisnik"]["Email"]}')
+    print(f'\tAdresa: {prodaja["Korisnik"]["Adresa"]["Grad"]}, {prodaja["Korisnik"]["Adresa"]["Ulica"]}, {prodaja["Korisnik"]["Adresa"]["Kucni_broj"]}')
+
+    print('Informacije o artiklu: ')
+    print(f'\t Naslov: {prodaja["Artikl"]["Naslov"]}')
+    print(f'\t Opis: {prodaja["Artikl"]["Opis"]}')
+    print(f'\t Cijena: {prodaja["Artikl"]["Cijena"]}')
+
+    print('Datum isteka prodaje: ')
+    print(f'\t Dan: {prodaja["Datum"].day}')
+    print(f'\t Mjesec: {prodaja["Datum"].month}')
+    print(f'\t Godina: {prodaja["Datum"].year}')
+
+    print('-'*30)
